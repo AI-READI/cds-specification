79bc8f4506a26c4da8fb593bf96e7c363dbea6a3
Sanjay Soundarajan <sanjay.16@live.com>
2022-05-11 11:40:43 -0700

    docs: 📚️ update commitlint types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,30 @@
-const conventionalCommit = require("./conventionalCommit.json");
+// const conventionalCommit = require("./conventionalCommit.json");
 
-const typesEnum = Object.keys(conventionalCommit.types);
-const scopesEnum = Object.keys(conventionalCommit.scopes);
+// const typesEnum = Object.keys(conventionalCommit.types);
+// const scopesEnum = Object.keys(conventionalCommit.scopes);
+
+const types = [
+  "build",
+  "chore",
+  "ci",
+  "docs",
+  "feat",
+  "fix",
+  "perf",
+  "refactor",
+  "revert",
+  "style",
+  "test",
+];
+
+const scopes = ["app", "library", "backend", "authentication", "settings"];
 
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "type-enum": [2, "always", typesEnum],
+    "type-enum": [2, "always", types],
     "scope-case": [2, "always", ["camel-case"]],
-    "scope-enum": [2, "always", scopesEnum],
+    "scope-enum": [2, "always", scopes],
     "subject-empty": [2, "never"],
     "subject-case": [2, "always", ["lower-case"]],
     "header-max-length": [2, "always", 72],
