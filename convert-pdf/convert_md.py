ba7922e9fad82c56cd096f44eba1b8dada62ac8d
aydawka <aydan.gasimova2@gmail.com>
2024-11-12 10:31:33 -0800

    feat: add pdf converter

---
+++ convert-pdf/convert_md.py
@@ -0,0 +1,35 @@
+import yaml
+from pathlib import Path
+import subprocess
+
+HERE = Path(__file__).absolute()
+
+def _flatten_values(lst):
+    """Flatten a list of dicts of lists to a list of values."""
+    for obj in lst:
+        for val in obj.values():
+            if isinstance(val, str):
+                yield val
+            else:
+                yield from _flatten_values(val)
+
+
+logfile ="cds-docs.log"
+filename ="cds-docs.pdf"
+# Prepare the command options
+cmd = [
+    "pandoc",
+    f"--log={logfile}",
+    f"--output={filename}",
+]
+
+build_root = HERE.parent.parent
+
+with open(build_root / "mkdocs.yml", "r") as stream:
+    mkdocs_yml = yaml.safe_load(stream)
+
+sections = mkdocs_yml["nav"]
+cmd += [str(build_root / "docs" / md) for md in _flatten_values(sections)]
+
+print("pandoc command being run: \n\n" + "\n".join(cmd))
+subprocess.run(cmd)
