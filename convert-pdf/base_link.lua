dcd8399da3fd1d0dc470c221ecb1bd3875a41c5b
aydawka <aydan.gasimova2@gmail.com>
2024-11-15 11:54:32 -0800

    fix: broken links

--- convert-pdf/base_link.lua
+++ convert-pdf/base_link.lua
@@ -6,10 +6,29 @@ function Link(el)
 
     if not el.target:match("^http") then
         if el.target:match("^%../") then
-            el.target = base_url .. "latest/schemas/" ..el.target
+            el.target = base_url .. "latest/schemas/" .. el.target
+        elseif el.target:match("^participants") or
+               el.target:match("^study%-description") or
+               el.target:match("^dataset%-description") then
+            el.target = base_url .. "specification/root-metadata-files/" .. el.target
+        elseif el.target:match("^general%--principles") then
+            el.target = base_url .. "specification/" .. el.target:gsub("%.md#", "#")
+        elseif el.target:match("^root%-metadata%-files/readme") or
+               el.target:match("^root%-metadata%-files/general") or
+               el.target:match("^root%-metadata%-files/dataset%-structure") or
+               el.target:match("^root%-metadata%-files/dataset%-description") or
+               el.target:match("^root%-metadata%-files/changelog") or
+               el.target:match("^root%-metadata%-files/healthsheet") or
+               el.target:match("^root%-metadata%-files/license") or
+               el.target:match("^root%-metadata%-files/participants") or
+               el.target:match("^root%-directory%-structure") or
+               el.target:match("^datatype%-directory") or
+               el.target:match("^participants") or
+               el.target:match("^root%-metadata%-files/study%-description") then
+            el.target = base_url .. "specification/" .. el.target
         else
             el.target = el.target:gsub("%.schema.json$", "")
-            el.target = base_url .. "specification/" .. el.target
+            el.target = base_url .. el.target
         end
     end
     return el
