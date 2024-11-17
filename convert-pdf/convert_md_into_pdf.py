a2976877d0fe2931c43374d75c753a43fdd0e585
aydawka <aydan.gasimova2@gmail.com>
2024-11-16 20:44:32 -0800

    fix: add comments

--- convert-pdf/convert_md_into_pdf.py
+++ convert-pdf/convert_md_into_pdf.py
@@ -16,28 +16,34 @@ def _flatten_values(lst):
 
 logfile ="cds-docs.log"
 filename ="cds-docs.pdf"
+title= "CDS Specification"
 # Prepare the command options
 cmd = [
     "pandoc",
-    # "--include-before-body=../../convert-pdf/cover.tex",
-    # "--include-in-header=../../convert-pdf/header.tex",
     f"--log={logfile}",
     f"--output={filename}",
+    # set 2 main paths
     "--resource-path=../docs/images",
+    # Fix relative urls using lua filter
     "--lua-filter=base_link.lua",
-    "--variable=title: CDS Specification",
+    # Set main and title page to CDS specification
+    f"--variable=title: {title}",
+    f"--metadata=title: {title}",
+    # Set Link colors
     "-V", "urlcolor=blue",
     "-V", "linkcolor=blue",
-    # "--toc"
 ]
 
 build_root = HERE.parent.parent
 
+#
 with open(build_root / "mkdocs.yml", "r") as stream:
     mkdocs_yml = yaml.safe_load(stream)
 
 sections = mkdocs_yml["nav"]
 cmd += [str(build_root / "docs" / md) for md in _flatten_values(sections)]
 
-print("pandoc command being run: \n\n" + "\n".join(cmd))
+# print("pandoc command being run: \n\n" + "\n".join(cmd))
+
+# Run the command
 subprocess.run(cmd)
